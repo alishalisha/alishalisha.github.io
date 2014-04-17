5f24b64c287b43b071b27695186db801d30ba439
Brian Anderson <banderson623@gmail.com>
2014-04-17 10:59:32 -0500

    slugs are now built from that page title

--- config.rb
+++ config.rb
@@ -37,14 +37,14 @@ spreadsheet_id = "0Aq4r9E2MjHrBdHdvTExMbEdoUDVqZTFISjFvazM4Smc"
 load_spreadsheet("index",  spreadsheet_id, :gid => 0)
 load_spreadsheet("week_1", spreadsheet_id, :gid => 13)
 load_spreadsheet("week_2", spreadsheet_id, :gid => 14)
-load_spreadsheet("week_3", spreadsheet_id, :gid => 15)
-load_spreadsheet("week_4", spreadsheet_id, :gid => 16)
-load_spreadsheet("week_5", spreadsheet_id, :gid => 17)
-load_spreadsheet("week_6", spreadsheet_id, :gid => 18)
-load_spreadsheet("week_7", spreadsheet_id, :gid => 19)
-load_spreadsheet("week_8", spreadsheet_id, :gid => 20)
-load_spreadsheet("week_9", spreadsheet_id, :gid => 21)
-load_spreadsheet("week_10", spreadsheet_id, :gid => 22)
+# load_spreadsheet("week_3", spreadsheet_id, :gid => 15)
+# load_spreadsheet("week_4", spreadsheet_id, :gid => 16)
+# load_spreadsheet("week_5", spreadsheet_id, :gid => 17)
+# load_spreadsheet("week_6", spreadsheet_id, :gid => 18)
+# load_spreadsheet("week_7", spreadsheet_id, :gid => 19)
+# load_spreadsheet("week_8", spreadsheet_id, :gid => 20)
+# load_spreadsheet("week_9", spreadsheet_id, :gid => 21)
+# load_spreadsheet("week_10", spreadsheet_id, :gid => 22)
 
 # Load the first index view
 weeks = [data.index]
@@ -65,6 +65,13 @@ published_weeks = weeks.select {|week_data|
                                                      row.title.to_s.downcase == "yes") }
 }
 
+week_slugs = []
+published_weeks.each_with_index do |week_data, index|
+  slug = week_data.detect{|row| row.kind == "page_title"}.fetch("title","week-#{(index+1)}")
+  slug = "#{index+1}/" + slug.split(/\W/).join('-') #make it url friendly
+  week_slugs << slug
+end
+
 puts "published_weeks count: #{published_weeks.length}"
 
 # uncomment to show all articles regardless of published state
@@ -72,15 +79,17 @@ puts "published_weeks count: #{published_weeks.length}"
 
 published_weeks.each_with_index do |week,week_index|
   human_index = week_index + 1
-  puts "Publishing week: #{human_index}"
-  proxy "/week-#{human_index}/index.html",
+  slug = week_slugs[week_index]
+  puts "Publishing week: #{human_index} slug: #{slug}"
+  proxy "/#{slug}/index.html",
         "main.html",
         :locals => {:weeks => published_weeks,
-                    :active_slug => "week-#{human_index}",
-                    :slug => "week-#{human_index}" }
+                    :active_slug => slug,
+                    :slug => slug,
+                    :slugs => week_slugs }
 end
 
-proxy "/index.html",'main.html', :locals => {:weeks => published_weeks}
+proxy "/index.html",'main.html', :locals => {:weeks => published_weeks, :slugs => week_slugs}
 
 # Build-specific configuration
 configure :build do
