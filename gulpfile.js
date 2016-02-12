9b72b2a8718f7a3ce177c98aec4c52f10f78d441
alishalisha <alisha.ramos@voxmedia.com>
2016-02-11 22:26:32 -0500

    gulpfile change

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,16 @@ var gulp                = require('gulp'),
 svgSprite               = require('gulp-svg-sprite');
 
 var config                  = {
+    shape                   : {
+        dimension           : {
+          maxWidth          : 50,
+          maxHeight         : 50
+        },
+        spacing             : {
+          padding           : 0,
+          box               : 'icon'
+        }
+    },
     svg                   : {
         xmlDeclaration    : false,
         doctypeDeclaration: false,
